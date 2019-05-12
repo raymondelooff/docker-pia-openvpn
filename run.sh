7b6ef3ce7e1753d6b26befa88873a6fcaf8aec14
Raymon de Looff <raydelooff@gmail.com>
2019-05-12 11:30:08 +0200

    Update iptables DNS rules

--- run.sh
+++ run.sh
@@ -62,10 +62,10 @@ iptables_setup() {
     fi
 
     # Allow traffic to DNS servers
-    iptables -A INPUT -p tcp --sport 53 -j ACCEPT
-    iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-    iptables -A INPUT -p udp --sport 53 -j ACCEPT
-    iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+    iptables -A INPUT -p tcp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -p udp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
+    iptables -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 
     # Allow traffic on loopback interface
     iptables -A INPUT -i lo -j ACCEPT
